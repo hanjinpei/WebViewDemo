4c6a907954c32871745691dba1e2f3de77f9fe2a
FNT <fnt@FNTs-iMac.local>
2013-05-24 16:42:48 +0800

    Initial Commit

---
+++ WebViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebViewDemo
+//
+//  Created by FNT on 5/24/13.
+//  Copyright (c) 2013 WebViewDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebViewDemo
+//
+//  Created by FNT on 5/24/13.
+//  Copyright (c) 2013 WebViewDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
